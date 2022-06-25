a150a0eb3db94f0bdad9d42b085bbf3dcf7a6751
Antoine Wood <antoinewood@gmail.com>
2022-06-25 16:26:04 -0400

    Add descriptions.py

--- descriptions.py
+++ descriptions.py
@@ -199,11 +199,8 @@ def main(args):
                         write(detections[i], orig_img, sampled_caption, sentence, i + 1, coco_classes, colors)
                         # list(map(lambda x: write(x, orig_img, captions), detections[i].unsqueeze(0)))
 
-        cv2.imshow("frame", orig_img)
-        key = cv2.waitKey(0)
-
-        if key & 0xFF == ord('q'):
-            break
+        filename = '/fashion-ai-analysis/save/' + image_filename[image_filename.rindex('/'):]
+        cv2.imwrite(filename, orig_img)
 
 
 #    image = Image.open(args.image)
